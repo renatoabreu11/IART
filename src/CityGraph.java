452ec06bfdc8625d183cd2451d1cfbcc4081ca19
renatoabreu11 <renatoabreu1196@gmail.com>
2017-03-26 19:07:28 +0100

    graph dgs files

--- src/CityGraph.java
+++ src/CityGraph.java
@@ -4,27 +4,31 @@ import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.file.FileSinkDGS;
 import org.graphstream.ui.view.Viewer;
 import org.graphstream.ui.view.ViewerListener;
 import org.graphstream.ui.view.ViewerPipe;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 public class CityGraph implements ViewerListener {
     private Graph graph;
-    private int averageDegree;
     protected boolean loop = true;
 
-    public CityGraph(String id, int averageDegree){
-        if(!(id.equals("Random") || id.equals("Barabàsi-Albert") || id.equals("Dorogovtsev mendes"))){
+    public CityGraph(String id){
+        graph = new SingleGraph(id);
+        readGraph(id);
+    }
+
+    public void generateGraph(String type, int averageDegree){
+        if(!(type.equals("Random") || type.equals("Barabàsi-Albert") || type.equals("Dorogovtsev mendes"))){
             System.out.println("Invalid type of graph");
             return;
         }
-        graph = new SingleGraph(id);
-        this.averageDegree = averageDegree;
-    }
-
-    public void generateGraph(){
         Generator gen = null;
-        switch(graph.getId()){
+        switch(type){
             case "Random":
                 gen = new RandomGenerator(averageDegree);
                 break;
@@ -43,10 +47,28 @@ public class CityGraph implements ViewerListener {
         gen.end();
     }
 
+    public void readGraph(String filename){
+        try {
+            graph.read("data/"+filename+".dgs");
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void saveGraph(){
+        try {
+            graph.write(new FileSinkDGS(), "data/" + graph.getId() + ".dgs");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void display(){
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         graph.addAttribute("ui.quality");
         graph.addAttribute("ui.antialias");
+        setStyleSheet("stylesheet");
         Viewer viewer = graph.display();
 
         ViewerPipe fromViewer = viewer.newViewerPipe();
@@ -57,8 +79,8 @@ public class CityGraph implements ViewerListener {
         }
     }
 
-    public void setGraphColour(String color){
-        graph.addAttribute("ui.stylesheet", "graph { fill-color: " + color + "; }");
+    public void setStyleSheet(String file){
+        graph.addAttribute("ui.stylesheet", "url(data/"+file+".css)");
     }
 
     public Graph getGraph() {
@@ -69,14 +91,6 @@ public class CityGraph implements ViewerListener {
         this.graph = graph;
     }
 
-    public int getAverageDegree() {
-        return averageDegree;
-    }
-
-    public void setAverageDegree(int averageDegree) {
-        this.averageDegree = averageDegree;
-    }
-
     @Override
     public void viewClosed(String viewName) {
         loop = false;
@@ -85,6 +99,15 @@ public class CityGraph implements ViewerListener {
     @Override
     public void buttonPushed(String id) {
         System.out.println("Button pushed on node "+id);
+        Map waste = graph.getNode(id).getAttribute("waste");
+        Iterator entries = waste.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry thisEntry = (Map.Entry) entries.next();
+            Object key = thisEntry.getKey();
+            Object value = thisEntry.getValue();
+            System.out.println(key);
+            System.out.println(value);
+        }
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-        CityGraph cg = new CityGraph("Dorogovtsev mendes", 2);
-        cg.generateGraph();
+        CityGraph cg = new CityGraph("graph1");
         cg.display();
     }
 }
